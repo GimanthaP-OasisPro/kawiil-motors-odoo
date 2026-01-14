ed99dbcc753631fc8d9e358e7856201a37db2f78
Gimantha <GimanthaP@oasispro.co.uk>
2026-01-14 23:12:40 +0530

    Add manifest for motorcycle_financing module

---
+++ odoo-trainings/motorcycle_financing/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Motorcycle Financing",
+    "summary": "Streamlines the loan application process for dealerships.",
+    "category": "Kawiil/Custom Modules",
+    "version": "0.0.1",
+    "website": "https://github.com/GimanthaP-OasisPro/kawiil-motors-odoo",
+    "author": "Gimantha Pathirana",
+    "license": "OPL-1",
+    "application": True,
+    "installable": True,
+    "depends": ["base"],
+    "data": [],
+}
